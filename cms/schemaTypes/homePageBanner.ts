7601d569dd863f6d5577329c887835600e66fe20
Skaps664 <msudaisk664@gmail.com.com>
2025-06-25 16:16:36 +0500

    CMS added and functions to import form it for Cateogry heading tested

---
+++ cms/schemaTypes/homePageBanner.ts
@@ -0,0 +1,40 @@
+const homePageBanner = {
+  name: 'homePageBanner',
+  title: 'Home Page Banner',
+  type: 'document',
+  fields: [
+    {
+      name: 'bannerText1',
+      title: 'Banner Text 1',
+      type: 'string',
+    },
+    {
+      name: 'bannerText2',
+      title: 'Banner Text 2',
+      type: 'string',
+    },
+    {
+      name: 'bannerText3',
+      title: 'Banner Text 3',
+      type: 'string',
+    },
+    {
+      name: 'bannerText4',
+      title: 'Banner Text 4',
+      type: 'string',
+    },
+    {
+      name: 'bannerText5',
+      title: 'Banner Text 5',
+      type: 'string',
+    },
+    {
+      name: 'bannerText6',
+      title: 'Banner Text 6',
+      type: 'string',
+    },
+    
+  ],
+}
+
+export default homePageBanner
